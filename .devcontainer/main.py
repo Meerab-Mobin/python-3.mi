f17b0b162a27558aaf1ec03b6fe36c20713bd700
Meerab-Mobin <mobinmeerab@gmail.com>
2024-08-18 12:31:42 +0000

    additional change

--- .devcontainer/main.py
+++ .devcontainer/main.py
@@ -1,9 +1,38 @@
-userName:int=10#camel case
-user_name:int=10#snake case
-UserName:int=10#pascalcode
-username:int=10#smallcase
-USERNAME:int=10#upper case
-#DEF for understanding
+#methods of assigning name to a variable : 
+
+userName:int=10 #camel case : first letter is small and first letter of second name is capital
+user_name:int=10 #snake case: all letter are small with underscore
+UserName:int=10 #pascalcode: first letters of all words is capital
+username:int=10 #smallcase : all letters are small
+USERNAME:int=10 #upper case: all letters are capital
+
+
+#Data types : 
+ #1) Integer : int ; 1,2 
+ #2) float : float; 1.1 , 2.9
+ #3) Boolean : bool ; true or false
+ #4) string : str ; "My name is Meerab"
+ #5) complex : complex ; pie , sigma , iota (i)
+
+
+#OPERATORS: 
+    #1) arithematic operators + ,- ,* /
+    #2) assignment operator =
+    #3) arithematic assignment operator += ,-= , /= , */ 
+    #4) comparison operator <, > , == ,!=    
+    # comparison operators: 
+        # greater > ,
+        # smaller < ,
+        # equals to == 
+        # not equals to !=
+        # these operators present answer in boolean( True or False)
+    #5) logical operators  AND ,NOT , OR
+    #6) unordinary operators :  
+        # Floor(//) : it will present the answer of division(quotient) in whole  numbers 
+        # Exponent(**): it will raise the power of number to given number e:g 2**3 = 8
+#7 int(input("enter a number":))
+#8   
+#practice:  
 #userName:str="nouraiz
 # num1:int=10
 #not:float=10.5
@@ -16,3 +45,31 @@ lastName:str="mobin"
 print(firstName+lastName)
 num3=num1-num2*6
 print(num3)
+
+print(id(num1))
+
+print(type(num3))
+
+a:int=55
+b:int=60
+c:int=60%55
+print(c)
+
+a=2
+b=5
+c=2**5
+print(c)
+
+c = 2//5
+print(c)
+
+a+=5
+a-=5
+a*=5
+a/=5
+a%=5
+
+#
+
+
+
