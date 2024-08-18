e21a3bbf047b2d6b7d37faa8ec359b147d385c16
Meerab-Mobin <mobinmeerab@gmail.com>
2024-08-18 10:47:16 +0000

    intcommit

---
+++ .devcontainer/main.py
@@ -0,0 +1,10 @@
+userName:int=10#camel case
+user_name:int=10#snake case
+UserName:int=10#pascalcode
+username:int=10#smallcase
+USERNAME:int=10#upper case
+#DEF for understanding
+#userName:str="nouraiz
+# num1:int=10
+#not:float=10.5
+#active:bool=true
