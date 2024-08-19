521eae8ddf04b84da8a8b135b8f78574150a717e
Meerab-Mobin <mobinmeerab@gmail.com>
2024-08-19 09:42:50 +0000

    intcommit

---
+++ .devcontainer/practice .py
@@ -0,0 +1,37 @@
+# problem no 1:your age 
+current_year = 2024
+birth_year = int(input("your birth year: "))
+your_age = current_year - birth_year
+print(your_age )
+
+#area of rectangle
+lenght=int(input("enter the lenght of rectangle"))
+width=int(input("enter width of ractangle "))
+area= lenght*width
+print(area)
+
+#area of circle 
+r = float(input("radius of the circle: "))
+pie_of_circle = float(input("pie:   "))
+area = (r**2)*pie_of_circle
+print(area)
+
+#area of cube
+#area=6a**2
+a =float(input("a is equal to: "))
+v=int(6)
+area_of_cube =(a**2)*v
+print(area_of_cube)
+
+#temp from F to C
+FRY=float(input("temp in F:  "))
+Celsius =(5) /(9)*(FRY)-(32)
+print(Celsius)
+
+#calculate a percentage
+obtain_number =float(input("obtain number:  "))
+total_number = float(input("total number:  "))
+percentage = (obtain_number)/(total_number)*(100)
+print(percentage)
+
+#
