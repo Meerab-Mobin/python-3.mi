cd87bc3c60483c29a38fa238559ea0b14deccc82
Meerab-Mobin <mobinmeerab@gmail.com>
2024-10-19 05:40:34 -0700

    c

--- .devcontainer/class fuctiov.py
+++ .devcontainer/class fuctiov.py
@@ -24,7 +24,6 @@ class Universty :
 alpa = Universty ()
 alpa.humans()
 alpa.faculty (10,20)
-alpa.faculty.x()
-alpa.faclty.y ()
+
 alpa.students_timings_1_7()
 alpa.classes_from_2_6()                                                                                                                                        
