16b364e03e3a06fa8cce7518e9cbb48811f4bb9d
Meerab-Mobin <mobinmeerab@gmail.com>
2024-10-19 04:59:01 -0700

    dik

--- .devcontainer/class fuctiov.py
+++ .devcontainer/class fuctiov.py
@@ -2,10 +2,10 @@ class Universty :
 
 
     def humans (self ):
-        print("universty students ")
+        print("universty students ") 
         print("universty teachers ")
 
-    def faculty  (self ):
+    def faculty  (self,x,y  ):
         print("teachers " )
         print("students")
 
@@ -13,12 +13,15 @@ class Universty :
     def students_timings_1_7  (self):
         studentnames:list [str] = [ "meerab", "minahil" , "areesha" ,"hamza" ,"ali" ,"iqra" ]
         print("students timing from 1_7 ", studentnames)
+    
+
+    def classes_from_2_6 (self):
+        terachers:list [str] = ["dr amin ", "dr tariq ", "dr khalid "]
+        st:list [str] = ["sulsabeel","haram","annaya ","mobin"]
 
 
-
-        
 alpa = Universty ()
 alpa.humans()
-alpa.faculty ()
+alpa.faculty (10,20)
 alpa.students_timings_1_7()
-                                                                                                                                           
+alpa.classes_from_2_6()                                                                                                                                        
