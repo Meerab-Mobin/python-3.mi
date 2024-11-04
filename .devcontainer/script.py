fba55884bbf4e8e2e005fbffb674c34dc2878fd2
Meerab-Mobin <mobinmeerab@gmail.com>
2024-11-03 18:04:37 -0800

    cc

--- .devcontainer/script.py
+++ .devcontainer/script.py
@@ -1,16 +1,9 @@
-print("hello world")
-print("its me")
-print("hii")
-#starting operations
-print(234*3293)
 
-print("get lostt al of you is my world")
-  def humans  (self): 
-        self.teachers  = str(input("universty teachers,   "))
-        self.students =str (input ("universty students , "))
+  
 
-    def display ():
-        print (self. teacher )
-        print (self.student)
-universty =  humans ()
-universty= display ()
+
+count:int = 1 
+while count <10 :
+    print ("hello merrab ",count)
+    count = count + 1
+  
