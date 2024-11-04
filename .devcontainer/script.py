0232c5672d20abf45126c94113c590466bab324e
Meerab-Mobin <mobinmeerab@gmail.com>
2024-11-03 18:13:36 -0800

    v

--- .devcontainer/script.py
+++ .devcontainer/script.py
@@ -2,8 +2,15 @@
   
 
 
-count:int = 1 
-while count <10 :
-    print ("hello merrab ",count)
-    count = count + 1
+ # count:int = 1 
+ ##while count <10 :
+    #print ("hello merrab ",count)
+    #count = count + 1
   
+
+num:int = input("enter the number ",   )
+count :int = 1 
+while count >= 10 :
+    print ("f {num} * {count} = ", num * count  )
+    count +=1 
+
