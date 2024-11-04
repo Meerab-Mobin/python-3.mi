0c2d405fd3fb11203c95678b3cf0f39f44bb3a63
Meerab-Mobin <mobinmeerab@gmail.com>
2024-11-03 18:37:28 -0800

    v

--- .devcontainer/script.py
+++ .devcontainer/script.py
@@ -8,9 +8,12 @@
     #count = count + 1
   
 
-num:int = input("enter the number ",   )
-count :int = 1 
-while count >= 10 :
-    print ("f {num} * {count} = ", num * count  )
+num:int = int(input("enter the number ",   ))
+count :int = 1  
+total:int = 0 
+while count <10 :
+    print (f"{num} * {count} =  ", num * count )
     count +=1 
 
+
+while 
